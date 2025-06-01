b8860351fa9e988487be3dd2a8a541527aa77f4b
vianetav <viane_tn@hotmail.com>
2025-06-01 12:37:49 -0300

    Update exerc2.c

--- exerc2.c
+++ exerc2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das Cartas
+// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
+// Siga os comentários para implementar cada parte do desafio.
+//Aluna Viviane Tavares Nascimento matricula 202504564977
+
+int main() {
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    //variaveis char
+    char estado[1],codigocarta[3],nomecidade[20];
+    //variaveis float
+    float area,pib;
+    //variaveis int
+    int populacao,turismo;
+        
+    // Cadastro das Cartas:
+    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
+    // Exibição dos Dados das Cartas:
+    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    return 0;
+}
